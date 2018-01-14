6d558bd2e1b9bab83cffc4d626f70ea572986b0b
Niko Arellano <narellano@my.bcit.ca>
2018-01-13 16:25:10 -0800

    Modified menu items

--- src/home.js
+++ src/home.js
@@ -26,9 +26,7 @@ export default class HomePage extends Component {
                     <Container>
                     <Menu inverted pointing secondary size='large'>
                         <Menu.Item as='a' active>Home</Menu.Item>
-                        <Menu.Item as='a'>Work</Menu.Item>
-                        <Menu.Item as='a'>Company</Menu.Item>
-                        <Menu.Item as='a'>Careers</Menu.Item>
+                        <Menu.Item as='a'>About Us</Menu.Item>
                         <Menu.Item position='right'>
                         <Button as='a' inverted>Log in</Button>
                         <Button as='a' inverted style={{ marginLeft: '0.5em' }}>Sign Up</Button>
