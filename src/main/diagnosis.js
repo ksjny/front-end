a235bb85bba4ea9711f17daf5c501c5aa06d48fe
Niko Arellano <narellano@my.bcit.ca>
2018-01-13 20:39:02 -0800

    added some pages and some more content

--- src/main/diagnosis.js
+++ src/main/diagnosis.js
@@ -11,20 +11,21 @@ import {
     Rating
 } from 'semantic-ui-react'
 
-export default class Dashboard extends Component {
+export default class Diagnosis extends Component {
     render() {
         return (
-            <Segment vertical style={styles.dashboardContainer}>
-                <Header size='huge'>Logs</Header>
+            <Segment vertical style={styles.diagnosisContainer}>
+                <Header size='huge'>Diagnosis</Header>
                 <Table celled padded>
                     <Table.Header>
                     <Table.Row textAlign="center">
                         <Table.HeaderCell singleLine>Time</Table.HeaderCell>
                         <Table.HeaderCell>Description</Table.HeaderCell>
-                        <Table.HeaderCell>Severity</Table.HeaderCell>
+                        {/*<Table.HeaderCell>Severity</Table.HeaderCell>*/}
                     </Table.Row>
                     </Table.Header>
                     <Table.Body>
+                    {/*
                     <Table.Row textAlign="center">
                         <Table.Cell>
                             <Header as='h5' textAlign='center'>10:40PM</Header>
@@ -38,16 +39,16 @@ export default class Dashboard extends Component {
                         </Table.Cell>
                         <Table.Cell singleLine>Weight</Table.Cell>
                         <Table.Cell>3</Table.Cell>
-                    </Table.Row>
+                    </Table.Row>*/}
                     </Table.Body>
                 </Table>
             </Segment>
-          )
+        )
     }
 }
 
 const styles = {
-    dashboardContainer : {
+    diagnosisContainer : {
         flexDirection : "column",
         width: "100%",
         minHeight : window.innerHeight
--- src/main/main.js
+++ src/main/main.js
@@ -4,23 +4,43 @@ import { Sidebar, Segment, Button, Menu, Image, Icon, Header, Container } from '
 
 export default class Main extends Component {
 
+    goToLogs = () => {
+      this.props.history.push('/main/dashboard')
+    }
+
+    goToMedicine = () => {
+      this.props.history.push('/main/medicine')
+    }
+
+    goToDiagnosis = () => {
+      this.props.history.push('/main/diagnosis')
+    }
+
+    goToProfile = () => {
+      this.props.history.push('/main/profile')
+    }
+
     render() {
         return (
               <Container>
                 <Sidebar as={Menu} width='thin' icon='labeled' visible={true} vertical inverted>
-                  <Menu.Item name='Logs'>
+                  <Menu.Item link name='Logs' onClick={this.goToLogs}>
                     <Icon name='file' />
                     Logs
                   </Menu.Item>
-                  <Menu.Item name='Medicine'>
+                  <Menu.Item link name='Medicine' onClick={this.goToMedicine}>
                     <Icon name='medkit' />
                     Medicine
                   </Menu.Item>
-                  <Menu.Item name='Profile'>
+                  <Menu.Item link name='Diagnosis' onClick={this.goToDiagnosis}>
+                    <Icon name='doctor' />
+                    Diagnosis
+                  </Menu.Item>
+                  <Menu.Item link name='Profile' onClick={this.goToProfile}>
                     <Icon name='user' />
                     Profile
                   </Menu.Item>
-                  <Menu.Item name='Sign Out'>
+                  <Menu.Item link name='Sign Out'>
                     <Icon name='log out' />
                     Sign Out
                   </Menu.Item>
